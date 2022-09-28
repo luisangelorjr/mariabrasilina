ce5a5aed4f11d72734eb9f70ef25e48f4f046c7b
luisangelorjr <luisangelorjr@gmail.com>
2022-09-28 00:09:05 -0300

    FIX: Remove template boilerplate

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-quickstart'`, () => {
+  it(`should have as title 'Biblioteca Maria Brasilina'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-quickstart');
+    expect(app.title).toEqual('Biblioteca Maria Brasilina');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-quickstart app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Biblioteca Maria Brasilina');
   });
 });
